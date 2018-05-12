4c66f6f1afb211232d08fa6f6955f6c6e0000a17
Sharon Yun <sharonyun11@gmail.com>
2018-05-12 19:12:48 -0400

    Finished student routes

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,55 @@
-const router = require("express").Router();
-const Student = require("../db/models/student");
+const router = require('express').Router();
+const Student = require('../db/models/student');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const students = await Student.findAll();
+    res.json(students);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    if (student) {
+      res.json(student);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const student = await Student.create(req.body);
+    res.status(201).json(student);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    const newStudent = await student.update(req.body);
+    res.json(newStudent);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    await student.destroy();
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
